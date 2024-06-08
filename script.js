7311021dba3f1e1bb1e73d820ee7665e7bb650fb
munergenis <154993411+munergenis@users.noreply.github.com>
2024-06-08 16:37:00 +0200

    project finished

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const output = document.getElementById("output")
 let isError = false
 
 addEntryButton.addEventListener("click", addEntry)
+calorieCounter.addEventListener("submit", calculateCalories)
+clearButton.addEventListener("click", clearForm)
 
 function cleanInputString(str) {
   const regex = /[+-\s]/g
@@ -51,6 +53,43 @@ function addEntry() {
   targetInputContainer.insertAdjacentHTML("beforeend", HTMLString)
 }
 
+function calculateCalories(e) {
+  e.preventDefault()
+  isError = false
+  
+  const breakfastNumberInputs = document.querySelectorAll("#breakfast input[type=number]")
+  const lunchNumberInputs = document.querySelectorAll("#lunch input[type=number]")
+  const dinnerNumberInputs = document.querySelectorAll("#dinner input[type=number]")
+  const snacksNumberInputs = document.querySelectorAll("#snacks input[type=number]")
+  const exerciseNumberInputs = document.querySelectorAll("#exercise input[type=number]")
+
+  const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs)
+  const lunchCalories = getCaloriesFromInputs(lunchNumberInputs)
+  const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs)
+  const snacksCalories = getCaloriesFromInputs(snacksNumberInputs)
+  const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs)
+  const budgetCalories = getCaloriesFromInputs([budgetNumberInput])
+
+  if (isError) return
+
+  const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories
+  const remainingCalories = budgetCalories - consumedCalories + exerciseCalories
+
+  const surplusOrDeficit = remainingCalories < 0 ? "Surplus" : "Deficit"
+
+  output.innerHTML = `
+    <span class="${surplusOrDeficit.toLowerCase()}">
+      ${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}
+    </span>
+    <hr>
+    <p>${budgetCalories} Calories Budgeted</p>
+    <p>${consumedCalories} Calories Consumed</p>
+    <p>${exerciseCalories} Calories Burned</p>
+  `
+
+  output.classList.remove("hide")
+}
+
 function getCaloriesFromInputs(list) {
   let calories = 0
   for (const item of list) {
@@ -61,6 +100,19 @@ function getCaloriesFromInputs(list) {
       isError = true
       return null
     }
-    calories += currVal
+    calories += Number(currVal)
   }
+  return calories
+}
+
+function clearForm() {
+  const inputContainers = Array.from(document.querySelectorAll(".input-container"))
+
+  for (const container of inputContainers) {
+    container.innerHTML = ""
+  }
+
+  budgetNumberInput.value = ""
+  output.innerText = ""
+  output.classList.add("hide")
 }
